704791228319984714626fff4d57e7cd61eeb386
haoli <haoli@ebay.com>
2012-09-27 15:41:41 +0800

    computeSCC correct commit

--- computeSCC.py
+++ computeSCC.py
@@ -144,4 +144,4 @@ threading.stack_size(67108864) # 64MB stack
 sys.setrecursionlimit(2 ** 20)  # approx 1 million recursions
 thread = threading.Thread(target = main) # instantiate thread object
 thread.start() # run program at target
-main()
+#main()
